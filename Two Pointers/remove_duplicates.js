cbffdd89ca8f1cdf8ed9f361a0d76d66c8134f84
zona-li <haoyang.zona@gmail.com>
2021-02-03 08:51:12 -0500

    add similar question

--- Two Pointers/remove_duplicates.js
+++ Two Pointers/remove_duplicates.js
@@ -13,3 +13,18 @@ function remove_duplicates(arr) {
 
   return nextNonDuplicate;
 }
+
+// Similar Question:
+// Given an unsorted array of numbers and a target ‘key’, remove all instances of ‘key’ in-place 
+// and return the new length of the array.
+
+function remove_element(arr, key) {
+  let nextElement = 0; // index of the next element which is not 'key'
+  for (i = 0; i < arr.length; i++) {
+    if (arr[i] !== key) {
+      arr[nextElement] = arr[i];
+      nextElement += 1;
+    }
+  }
+  return nextElement;
+}
