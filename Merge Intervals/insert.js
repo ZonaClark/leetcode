6d4ffb432d5450fa915c5c092ab9d9af04dbfb42
zona-li <haoyang.zona@gmail.com>
2021-02-07 10:54:50 -0500

    insert new interval

---
+++ Merge Intervals/insert.js
@@ -0,0 +1,51 @@
+class Interval {
+  constructor(start, end) {
+    this.start = start;
+    this.end = end;
+  }
+
+  print_interval() {
+    process.stdout.write(`[${this.start}, ${this.end}]`);
+  }
+}
+
+const insert = function(intervals, new_interval) {
+  let merged = [],
+  i = 0;
+
+  // skip and add to output) all intervals that come before the 'new_interval'
+  while (i < intervals.length && intervals[i].end < new_interval.start) {
+    merged.push(intervals[i]);
+    i += 1;
+  }
+
+  // merge all intervals that overlap with 'new_interval'
+  while (i < intervals.length && intervals[i].start <= new_interval.end) {
+    new_interval.start = Math.min(intervals[i].start, new_interval.start);
+    new_interval.end = Math.max(intervals[i].end, new_interval.end);
+    i += 1;
+  }
+
+  // insert the new_interval
+  merged.push(new_interval);
+
+  // add all the remaining intervals to the output
+  while (i < intervals.length) {
+    merged.push(intervals[i]);
+    i += 1;
+  }
+
+  return merged;
+};
+
+process.stdout.write('Intervals after inserting the new interval: ');
+result = insert([
+  new Interval(1, 3),
+  new Interval(5, 7),
+  new Interval(8, 12),
+], new Interval(4, 10));
+for (i = 0; i < result.length; i++) {
+  result[i].print_interval();
+}
+// Output: [[1,3], [4,12]]
+console.log();
